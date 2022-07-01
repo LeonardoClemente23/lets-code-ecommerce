4b3c460404902928f9cefe5594ca12b1ad7c5b6d
ThaisMK <thais.konishi@gmail.com>
2022-07-01 19:00:36 -0300

    Criando classes produtos

--- src/App.java
+++ src/App.java
@@ -1,5 +1,12 @@
+import produtos.Calcado;
+import produtos.Vestuario;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Vestuario blusa1 = new Vestuario(Vestuario.Tamanho.P, 111, "blusa descolada", 70.5d);
+        System.out.println(blusa1.toString());
+
+        Calcado tenis1 = new Calcado(37, 2001, "Asics run", 652.9);
+        System.out.println(tenis1.toString());
     }
 }

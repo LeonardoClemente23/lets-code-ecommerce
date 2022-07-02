103e5d2b2068c4f326599d60968a2339f2417190
Jose Erivaldo Oliveira Junior <jose.erivaldo@bb.com.br>
2022-07-02 12:28:01 -0300

    Iniciando implementacao da classe Compra

--- src/Compra.java
+++ src/Compra.java
@@ -1,3 +1,39 @@
-﻿public class Compra {
+﻿import java.time.LocalDate;
+
+public class Compra {
+    private Cliente cliente;
+    private Carrinho carrinho;
+    //Forma de pagamento
+    private LocalDate dataCompra;
+
+    public Compra(Cliente cliente, Carrinho carrinho) {
+        this.cliente = cliente;
+        this.carrinho = carrinho;
+        setDataCompra();
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public int getIdCarrinho() {
+        return carrinho.getId();
+    }
+
+    public void setCarrinho(Carrinho carrinho) {
+        this.carrinho = carrinho;
+    }
+
+    public LocalDate getDataCompra() {
+        return dataCompra;
+    }
+
+    private void setDataCompra() {
+        this.dataCompra = LocalDate.now();
+    }
 
 }

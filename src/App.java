52bfa8a81a2b97eb8dcba150c1c9aecc697a3e64
F9539591 Vinicius de Sousa Santana <vinicius.ssantana@bb.com.br>
2022-07-04 15:04:26 -0300

    Refatoração dos testes de produto

--- src/App.java
+++ src/App.java
@@ -5,14 +5,7 @@ import produtos.Vestuario;
 public class App {
     public static void main(String[] args) throws Exception {
 
-        Vestuario blusa1 = new Vestuario(Vestuario.Tamanho.P, 111, "blusa descolada", 70.5d);
-        System.out.println(blusa1.toString());
-
-        Calcado tenis1 = new Calcado(37, 2001, "Asics run", 652.9);
-        System.out.println(tenis1.toString());
-
-        Eletronico relogio1 = new Eletronico("preto", 3001, "Polar M600", 1500.0);
-        System.out.println(relogio1.toString());
+        testesProduto();
 
         testesCliente();
 
@@ -27,6 +20,19 @@ public class App {
         System.out.println(cliente2.getCpfFormatado());
         System.out.println(cliente1.getCpfLimpo());
         System.out.println(cliente2.getCpfLimpo());
+
+    }
+
+    public static void testesProduto() {
+
+        Vestuario blusa1 = new Vestuario(Vestuario.Tamanho.P, 111, "blusa descolada", 70.5d);
+        System.out.println(blusa1.toString());
+
+        Calcado tenis1 = new Calcado(37, 2001, "Asics run", 652.9);
+        System.out.println(tenis1.toString());
+
+        Eletronico relogio1 = new Eletronico("preto", 3001, "Polar M600", 1500.0);
+        System.out.println(relogio1.toString());
 
     }
 

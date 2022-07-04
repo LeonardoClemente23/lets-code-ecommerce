6dee6c58bcb1e4120986cd27852f4f97e8bfb5e4
F9539591 Vinicius de Sousa Santana <vinicius.ssantana@bb.com.br>
2022-07-04 19:30:01 -0300

    Estrutura inicial da loja

---
+++ src/produtos/Perfumaria.java
@@ -0,0 +1,28 @@
+﻿package produtos;
+
+public class Perfumaria extends Produto {
+
+    private String fragrancia;
+
+    public Perfumaria(String fragrancia, int idProduto, String nome, double preco) {
+        this.fragrancia = fragrancia;
+        super.setIdProduto(idProduto);
+        super.setNome(nome);
+        super.setPreco(preco);
+    }
+
+    public String getFragrancia() {
+        return fragrancia;
+    }
+
+    public void setFragrancia(String fragrancia) {
+        this.fragrancia = fragrancia;
+    }
+
+    @Override
+    public String toString() {
+        return "Eletronico [nome:" + super.getNome() + ",id:" + super.getIdProduto() + ",preço: " + super.getPreco()
+                + ",cor:" + this.fragrancia + "]";
+    }
+
+}

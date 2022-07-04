c1b0bc6a988067527e925413f8f04add42fd0e0b
f6206672 Leonardo Clemente de Figueiredo Silva <leonardo.clemente@bb.com.br>
2022-07-04 14:48:24 -0300

    Início da implementação das formas de pagamento e aprovação da compra

---
+++ src/RegrasFormaDePagamento/FormaPagamento.java
@@ -0,0 +1,18 @@
+package RegrasFormaDePagamento;
+
+public enum FormaPagamento {
+    BOLETO(new Pagamento()),
+    CARTAO_PARCELADO(new CartaoParcelado()),
+    CARTAO_A_VISTA(new Pagamento()),
+    PAYPAL(new Pagamento());
+
+    private Aprovacao tipo;
+
+    FormaPagamento(Aprovacao tipo) {
+        this.tipo = tipo;
+    }
+
+    public Aprovacao getFormaPagamento() {
+        return tipo;
+    }
+}

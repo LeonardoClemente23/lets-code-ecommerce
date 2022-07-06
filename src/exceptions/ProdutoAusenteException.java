561dc2806734a63cd870131b92393969b3d0386a
F9539591 Vinicius de Sousa Santana <vinicius.ssantana@bb.com.br>
2022-07-06 16:46:30 -0300

    Recriando classe ProdutoAusenteException

---
+++ src/exceptions/ProdutoAusenteException.java
@@ -0,0 +1,11 @@
+﻿package exceptions;
+
+public class ProdutoAusenteException extends RuntimeException {
+
+    public ProdutoAusenteException() {
+
+        super("Produto não existe no carrinho!");
+
+    }
+
+}
